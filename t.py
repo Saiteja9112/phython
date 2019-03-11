95cbd0b24d8e08b2f5a62befd6069243fe27c2d4
Saiteja9112 <47886206+Saiteja9112@users.noreply.github.com>
2019-03-11 14:42:10 +0530

    Add files via upload

---
+++ t.py
@@ -0,0 +1,10 @@
+creatr an empty list and print it
+assign elements to list and print it 
+print individual elements of the list 
+print elements of the list using for loop
+print elements of the list using the range function
+replace the value of the list and print it
+find the length of the list
+checking whether an element is present in list
+add an element to the list
+randomfn,choice fn,input fn'''
